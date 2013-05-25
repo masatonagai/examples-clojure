e730c2e5c742c05d250e79b15d90f8e9fb775dd9
Masato Nagai <me@masatonagai.com>
2013-05-26 04:51:22 +0900

    update

--- euler_problem_6.clj
+++ euler_problem_6.clj
@@ -1,7 +1,7 @@
 ;; The sum of the squares of the first ten natural numbers is,
 ;; 1^2 + 2^2 + ... + 10^2 = 385
 ;; The square of the sum of the first ten natural numbers is,
-;; (1 + 2 + ... + 10)^2 = 552 = 3025
+;; (1 + 2 + ... + 10)^2 = 55^2 = 3025
 ;; Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 ;; Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
@@ -9,20 +9,18 @@
   (* n n))
 
 (defn sum-of-squares [coll]
-  (reduce
-    +
-    (map square coll)))
+  (reduce + (map square coll)))
 
 (assert (== 385 (sum-of-squares (range 1 11))))
 
 (defn square-of-sum [coll]
-  (square
-    (reduce
-      +
-      coll)))
+  (square (reduce + coll)))
 
 (assert (== 3025 (square-of-sum (range 1 11))))
 
-(println
-  (let [coll (range 1 101)]
-    (- (square-of-sum coll) (sum-of-squares coll))))
+(defn answer [coll]
+  (- (square-of-sum coll) (sum-of-squares coll)))
+
+(assert (== 2640 (answer (range 1 11))))
+
+(println (answer (range 1 101)))
